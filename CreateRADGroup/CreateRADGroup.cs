94409854ad9c7540ff4867906f1324f137c610dc
Tobe Deprez <tobe.deprez@skyline.be>
2025-11-25 16:04:22 +0100

    Add script to create RAD group

---
+++ CreateRADGroup/CreateRADGroup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Skyline.DataMiner.Analytics.DataTypes;
+using Skyline.DataMiner.Analytics.Rad;
+using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+
+namespace CreateRADGroup
+{
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			try
+			{
+				RunSafe(engine);
+			}
+			catch (ScriptAbortException)
+			{
+				// Catch normal abort exceptions (engine.ExitFail or engine.ExitSuccess)
+				throw; // Comment if it should be treated as a normal exit of the script.
+			}
+			catch (ScriptForceAbortException)
+			{
+				// Catch forced abort exceptions, caused via external maintenance messages.
+				throw;
+			}
+			catch (ScriptTimeoutException)
+			{
+				// Catch timeout exceptions for when a script has been running for too long.
+				throw;
+			}
+			catch (InteractiveUserDetachedException)
+			{
+				// Catch a user detaching from the interactive script by closing the window.
+				// Only applicable for interactive scripts, can be removed for non-interactive scripts.
+				throw;
+			}
+			catch (Exception e)
+			{
+				engine.ExitFail("Run|Something went wrong: " + e);
+			}
+		}
+
+		private void RunSafe(IEngine engine)
+		{
+			var dms = engine.GetDms();
+
+			var subgroupInfos = dms.GetElements()
+				.Where(e => e.Name.StartsWith("AI - RAD - Commatia"))
+				.Select(e => new RADSubgroupInfo(e.Name, new List<RADParameter>()
+				{
+					new RADParameter(new ParameterKey(e.DmsElementId.AgentId, e.DmsElementId.ElementId, 2243, "PA1"), "PA1"),
+					new RADParameter(new ParameterKey(e.DmsElementId.AgentId, e.DmsElementId.ElementId, 2243, "PA2"), "PA2"),
+					new RADParameter(new ParameterKey(e.DmsElementId.AgentId, e.DmsElementId.ElementId, 2243, "PA3"), "PA3"),
+					new RADParameter(new ParameterKey(e.DmsElementId.AgentId, e.DmsElementId.ElementId, 1022), "Total Output Power"),
+				}))
+				.ToList();
+			var groupInfo = new RADGroupInfo("AI - RAD - Commtia", subgroupInfos, false);
+			var request = new AddRADParameterGroupMessage(groupInfo);
+			engine.SendSLNetMessage(request);
+		}
+	}
+}
